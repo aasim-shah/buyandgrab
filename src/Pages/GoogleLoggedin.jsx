ccdf154cf13c02f36020e2e821e2bd3de08ea567
Aasim shah <asimshah8110@gmail.com>
2022-11-21 23:18:01 +0500

    lasdfj

--- src/Pages/GoogleLoggedin.jsx
+++ src/Pages/GoogleLoggedin.jsx
@@ -3,11 +3,15 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import { HashLoader } from 'react-spinners'
 function GoogleLoggedin() {
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
 
 const getGoogleAuthUser = async()=>{
+    setIsLoading(true)
     const {data} = axios.get("https://ennmart.herokuapp.com/auth/success" , {withCredentials:true})
     console.log(data)
+    if(data.success){
+        setIsLoading(false)
+    }
 }
 
 useEffect(()=>{
@@ -15,7 +19,7 @@ useEffect(()=>{
 },[])
 
   return (
-    <div className="flex">
+    <div className="text-center font-bold bg-[#355b7d] flex justify-center items-center h-[100vh]">
           <HashLoader color={"#355b7d"} loading={isLoading} size={150} />
     </div>
   )
